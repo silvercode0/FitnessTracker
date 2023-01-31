ab6c1acac0ec4d4c469cae18e2d8a42116de0752
Silver <jice391@gmail.com>
2023-01-31 10:39:36 -0500

    updated files

--- db/seedData.js
+++ db/seedData.js
@@ -3,13 +3,60 @@
 const client = require("./client")
 
 async function dropTables() {
-  console.log("Dropping All Tables...")
-  // drop all tables, in the correct order
+  try {
+    console.log("Dropping All Tables...")
+
+    await client.query(`
+    DROP TABLE IF EXISTS routine_activities;
+    DROP TABLE IF EXISTS routines;
+    DROP TABLE IF EXISTS activities;
+    DROP TABLE IF EXISTS users;
+    `)
+
+    console.log("DOne Droping Tables")
+  } catch (error) {
+    throw error;
+  }
+  // drop all tables, in the correct order '
+  
 }
 
 async function createTables() {
-  console.log("Starting to build tables...")
+
+  try {
+    console.log("Starting to build tables...")
   // create all tables, in the correct order
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE activities (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL
+      );
+      CREATE TABLE routines (
+        id SERIAL PRIMARY KEY,
+        "creatorId" INTEGER REFERENCES users(id),
+        "isPublic" BOOLEAN DEFAULT false,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULL
+      );
+      CREATE TABLE routine_activities (
+        id SERIAL PRIMARY KEY,
+        "routineId" INTEGER REFERENCES routines (id),
+        "activityId" INTEGER REFERENCES activities (id),
+        duration INTEGER, 
+        count INTEGER,
+        UNIQUE ("routineId","activityId")
+      );
+    `)
+    console.log("Done Creating Tables")
+  } catch (error) {
+    throw error
+  }
 }
 
 /* 
--- db/users.js
+++ db/users.js
@@ -4,6 +4,12 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
+  try {
+    console.log("Creating USERS to use")
+  } catch (error) {
+    
+  }
+   
   
 }
 
@@ -11,11 +17,41 @@ async function getUser({ username, password }) {
 
 }
 
-async function getUserById(userId) {
+async function getUserById(id) {
+  try {
+    console.log("Getting User By Id")
+    const { rows: [ user ] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE id=${ id }
+    `);
+
+    if (!user) {
+      return null
+    } else {
+      delete user.password;
+      return user;
+    }
+    
+  } catch (error) {
+    console.log("Error Trying to Get User By Id")
+    throw error;
+  }
 
 }
 
 async function getUserByUsername(userName) {
+  try {
+    console.log("Getting User By Username")
+    const { rows: [ user ] } = await client.query(`
+      SELECT * 
+      FROM username
+      WHERE user=${ user }
+    `)
+  } catch (error) {
+    console.log("Error Getting User By Username")
+    throw error;
+  }
 
 }
 
