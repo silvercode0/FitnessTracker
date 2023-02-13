16eadec1d1a9f21b266a0b306a5e8a5cfaf1affd
Silver <jice391@gmail.com>
2023-02-12 23:51:03 -0500

    rebecca updated activity.js

--- api/activities.js
+++ api/activities.js
@@ -58,8 +58,67 @@ router.get('/', async (req, res) => {
     res.send({ activities });
 });
 
-// POST /api/activities
+router.get('/:activityId/routines', async (req, res, next) => {
+    
+    try {
+        
+        const id = req.params.activityId;
+        const activity = { id: id };
+        const routines = await getPublicRoutinesByActivity(activity);
+        
+        if (routines.length === 0)
+        
+        res.send({
+            message: `Activity ${id} not found`,
+            name: 'ActivityDoesNotExistError',
+            error: 'Activity does not exist',
+        });
+        
+        res.send(routines);
+    } catch ({ name, message }) {
+        
+        next({ name, message });
+    }
+});
 
-// PATCH /api/activities/:activityId
+
+router.patch('/:activityId', requireUser, async (req, res, next) => {
+    
+    const { activityId } = req.params;
+
+    const { name, description } = req.body;
+
+    const updateFields = {};
+    
+    if (name) {
+        
+        updateFields.name = name;
+    };
+    
+    if (description) {
+        
+        updateFields.description = description;
+    };
+
+    try {
+        
+        if (req.user) {
+            
+            const updatedActivity = await updateActivity(activityId, updateFields);
+            
+            res.send({ activity: updatedActivity });
+        } else {
+            
+            next({
+                name: "UserNotLoggedIn",
+                message: "Login to update activity",
+            });
+        }
+    
+    } catch ({ name, description }) {
+        
+        next({ name, description });
+    }
+});
 
 module.exports = router;
